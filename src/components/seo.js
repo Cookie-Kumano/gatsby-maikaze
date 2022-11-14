23222edc136412de685b7653666e4c8de3837409
Cookie-Kumano <cookie@kumano.moe>
2022-11-14 14:34:03 +0900

    deleted unnecessary package

--- src/components/seo.js
+++ src/components/seo.js
@@ -7,8 +7,6 @@
  */
 
 import * as React from "react"
-import PropTypes from "prop-types"
-import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
 const Seo = ({ description, lang, meta, title }) => {
