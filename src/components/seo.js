f6148a664b93853adbfe6b4725db032fe8006f37
Cookie-Kumano <cookie@kumano.moe>
2022-11-14 14:37:15 +0900

    fixed variable

--- src/components/seo.js
+++ src/components/seo.js
@@ -9,7 +9,7 @@
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
-const Seo = ({ description, lang, meta, title }) => {
+const Seo = ({ description, title, children }) => {
   const { site } = useStaticQuery(
     graphql`
       query {
